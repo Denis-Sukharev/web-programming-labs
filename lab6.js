9fce56bd980296f5ad4d7ffdf04b633aedd20b9e
Студент Факультета Бизнеса <student@FB.LOCAL>
2022-12-06 16:11:42 +0700

    Alert

--- lab6.js
+++ lab6.js
@@ -39,3 +39,33 @@ function setEnglish() {
 
     document.getElementById('reset-button').value = 'reset';
 }
+
+function showAlert() {
+    let userName = document.getElementById('input-name').value;
+    let userSurname = document.getElementById('input-surname').value;
+    let userAge = document.getElementById('input-age').value;
+    alert('Привет, ' + userName + ' ' + userSurname + '\n' + 'Ваш возраст: ' + userAge);
+    let r = Math.round(Math.random()*255);
+    let g = Math.round(Math.random()*255);
+    let b = Math.round(Math.random()*255);
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('user-data').style.backgroundColor = color;
+}
+
+function showAlert1() {
+    alert('Шапка');
+    let r = Math.round(Math.random()*255)
+    let g = Math.round(Math.random()*255)
+    let b = Math.round(Math.random()*255)
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('student').style.backgroundColor = color;
+}
+
+function showAlert2() {
+    alert('Подвал');
+    let r = Math.round(Math.random()*255)
+    let g = Math.round(Math.random()*255)
+    let b = Math.round(Math.random()*255)
+    let color = 'rgb(' + r + ',' + g + ',' + b + ')';
+    document.getElementById('footer').style.backgroundColor = color;
+}
