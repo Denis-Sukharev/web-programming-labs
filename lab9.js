01387556bbc0ef3768646ed04f7073eb08524834
Scopule <glebka.vershinin@gmail.com>
2022-12-23 10:40:07 +0700

    Вывод координат мыши

--- lab9.js
+++ lab9.js
@@ -11,3 +11,14 @@ function clickFIO(button) {
     else
         button.innerHTML = 'Денис Сухарев';
 }
+
+function showCoords(event) {
+    event.target.innerHTML = 'x = ' + event.offsetX + ' ' + ', y = ' + event.offsetY + ' ' + 
+    (event.ctrlKey? '<b>Ctrl</b>':'<s>Ctrl</s>') + ' ' + (event.shiftKey? '<b>Shift</b>':'<s>Shift</s>') + 
+    ' ' + (event.altKey? '<b>Alt</b>':'<s>Alt</s>' );
+}
+
+window.addEventListener('DOMContentLoaded', function() {
+    let coords = document.querySelector('#coords');
+    coords.addEventListener('mousemove', showCoords);
+});
