6537687c4c16c11ab30dd5f69a8665d73b01c32e
Scopule <glebka.vershinin@gmail.com>
2022-12-23 10:22:10 +0700

    Кнопка Имя-Фамилия

--- lab9.js
+++ lab9.js
@@ -4,3 +4,10 @@ function clickMe(button) {
     else
         button.innerHTML = 'Щелчок';
 }
+
+function clickFIO(button) {
+    if(button.innerHTML == 'Денис Сухарев')
+        button.innerHTML = 'Сухарев Денис';
+    else
+        button.innerHTML = 'Денис Сухарев';
+}
